bcba78dfc2f8426fc9ed816fa59851cb311c383a
DanielPichette <daniel.f.pichette@gmail.com>
2021-04-27 10:02:22 -0500

    problem 3a.

--- linkedlist.py
+++ linkedlist.py
@@ -4,6 +4,7 @@ from node import Node
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def append_node(self, data):
         node = Node(data)
@@ -18,3 +19,19 @@ class LinkedList:
             temporary_node = temporary_node.next
 
         temporary_node.next = node
+# PROBLEM 3:
+# ake the already created linked list with an append_node method and add on the following functionality:
+# add_to_beginning:
+
+    def add_to_beginning(self, data):
+        node = Node(data)
+
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            return
+        else:
+            if self.head is not None:
+                node.next =self.head
+                self.head = node
+                return
